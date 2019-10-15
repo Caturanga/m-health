b84e5a241f431d0564c9fdb829edec56e824d7f5
ululazmisetiawan <ululazmisetiawan@gmail.com>
2019-10-15 17:21:52 +0700

    tambahan rouded button dan login

---
+++ app/src/main/java/com/project/m_health/chart.java
@@ -0,0 +1,65 @@
+package com.project.m_health;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class chart extends AppCompatActivity {
+
+    PieChart pieChart, pieChart1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pieChart = (PieChart) findViewById(R.id.piechart);
+
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        yValues.add(new PieEntry(34f, "Bangladesh"));
+        yValues.add(new PieEntry(23f, "USA"));
+        yValues.add(new PieEntry(14f, "UK"));
+        yValues.add(new PieEntry(35, "India"));
+        yValues.add(new PieEntry(40, "Russia"));
+        yValues.add(new PieEntry(23, "Japan"));
+
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
+
+
+
+    }
+
+}
